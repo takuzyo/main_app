66cb9407581185c583736edbe5790cd519ca64d9
mii <mii012345@outlook.jp>
2021-09-05 11:27:17 +0900

    謎解きクラスの骨組み修正

--- mystery.py
+++ mystery.py
@@ -1,10 +1,28 @@
+#リクエストクラスなんでも可
 import urllib.request, urllib.parse
+
 
 class Mystery:
 
     def ___init__(self):
-        self.mystery_url = ''
+        #tokenは編集してね♡
+        self.TOKEN = 1
+        self.mystery_url = 'https://????.??/api/game/'
+        #現在の問題番号
+        self.now_mystery_id = 1
     
     def get_mystery(self):
+        """
+        謎解き取得
+        TODO: 取得
+        """
+        request_url = self.mystery_url
         image = ''
         return image
+    
+    def check_answer(self):
+        """
+        正誤判定
+        TODO: 判定
+        """
+        return False
